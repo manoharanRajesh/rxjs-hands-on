17422633342dc9b3d5f9985ec17895ad28ca6aba
manoharanRajesh <mano.rajesh@gmail.com>
2021-12-27 12:53:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-hands-on
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-hands-on)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  implements OnInit{
+  name = 'Angular ' + VERSION.major;
+  ngOnInit(): void {
+    console.log(`Learn RXJS Basic`);
+    
+  
+  }
+
+
+}
