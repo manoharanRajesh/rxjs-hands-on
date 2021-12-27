c28f4a4ce4b00f6c193e118cec09f7565d9b17a8
manoharanRajesh <mano.rajesh@gmail.com>
2021-12-27 14:42:12 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, VERSION } from '@angular/core';
 import { from, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -24,6 +25,21 @@ export class AppComponent implements OnInit {
         error: (err) => console.log(`From Err has occurred: ${err}`),
         complete: () => console.log(`From Compleated`)
       }
-    )
+    );
+    console.log(`Learn operators in rxjs https://rxjs.dev/api `);
+    of(1,2,3)
+    .pipe(
+      map(value => value*2),
+      map(value => value-3)
+    ).subscribe(console.log);
+
+    console.log(`Learn operators for debug `);
+    of(1,2,3)
+    .pipe(
+      map(value => value*2),
+      tap(value => console.log(`Debug log ${value}`)),
+      map(value => value-3)
+    ).subscribe(console.log);
+
   }
 }
