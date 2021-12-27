312e96a9fa5a88fba489d00ec942f1bcbcbd8ad5
manoharanRajesh <mano.rajesh@gmail.com>
2021-12-27 13:06:11 +0100

    learn RXJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit, VERSION } from '@angular/core';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
   ngOnInit(): void {
     console.log(`Learn RXJS Basic`);
-    
-  
+    const data = [1, 2, 3];
+    console.log(`Create Observables, observer and subcription`);
+    of(...data).subscribe({
+      next: (value) => console.log(`Next value ${value}`),
+      error: (err) => console.log(`Err has occurred: ${err}`),
+      complete: () => console.log(`Compleated`)
+    });
+
+    from(data).subscribe(
+      {
+        next: (value) => console.log(`From Next value ${value}`),
+        error: (err) => console.log(`From Err has occurred: ${err}`),
+        complete: () => console.log(`From Compleated`)
+      }
+    )
   }
-
-
 }
